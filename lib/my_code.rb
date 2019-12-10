988f919fb24cca5e26f883823f9bd76190986023
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-10 15:01:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,3 +19,14 @@ def map_to_no_change(source)
   end 
   return new_array
 end
+
+# My Code here....
+def map_to_double(source)
+  i = 0 
+  new_array = []
+  while i < source.length 
+    new_array.push(source[i]*2)
+    i+=1 
+  end 
+  return new_array
+end
